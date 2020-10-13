2fe8963c877e125959acbc46334a2b1b78e5804f
Bugsy <pchriste@microsoft.com>
2020-10-13 19:14:49 +0200

    Initial commit

---
+++ PerfectPitchPoC/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace PerfectPitchPoC
+{
+    public partial class MainPage : ContentPage
+    {
+        ISimpleAudioPlayer simpleAudioPlayer; 
+        public MainPage()
+        {
+            simpleAudioPlayer = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            simpleAudioPlayer.Loop = false;
+            bool ret = simpleAudioPlayer.Load(GetStreamFromFile($"Audio.G-0.wav"));
+            InitializeComponent();
+        }
+
+
+        Stream GetStreamFromFile(string filename)
+        {
+            var assembly = typeof(App).GetTypeInfo().Assembly;
+            var stream = assembly.GetManifestResourceStream("PerfectPitchPoC." + filename);
+
+            return stream;
+        }
+        void OnStartButtonClicked(object sender, EventArgs e)
+        {
+            simpleAudioPlayer.Play();
+        }
+
+        void OnStopButtonClicked(object sender, EventArgs e)
+        {
+            simpleAudioPlayer.Stop();
+        }
+    }
+}
